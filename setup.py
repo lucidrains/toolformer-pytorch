9f7878398fb13609c8a3ca088de5a6edef561de8
Phil Wang <lucidrains@gmail.com>
2023-04-04 15:11:14 -0700

    a function needed for the final sampling with api calling

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'toolformer-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Toolformer - Pytorch',
   author = 'Phil Wang',
